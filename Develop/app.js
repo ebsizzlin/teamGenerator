44a3a2d33a9334e73610e447e1653f3265f5ed75
Ella Thompson <fang98t@gmail.com>
2020-11-17 14:46:46 -0500

    adding readme screenshot

--- Develop/app.js
+++ Develop/app.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { create } = require("domain");
+const { create } = require("domain"); //idk what this is
 const team = []; //store everything
 
 //prompt for which set of questions
