036829d757b7e27b9f6054166b57b031754a1115
Ella Thompson <fang98t@gmail.com>
2020-11-17 11:32:59 -0500

    intern.js formatted and passing

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -8,16 +8,16 @@ class Employee  {
         this.id = id;
         this.email = email;
         this.role = 'Employee'
-    };
+    }
     getName()   {
         return this.name
-    };
+    }
     getId()   {
         return this.id
-    };
+    }
     getEmail()   {
         return this.email
-    };
+    }
     getRole()   {
         return this.role
     }
